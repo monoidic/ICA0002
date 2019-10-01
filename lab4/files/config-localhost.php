1bd1c81a45cf32f3a6c81f4ccd57a6a779380303
Mikk Margus Möll <mimoll@ttu.ee>
2019-10-02 02:28:18 +0300

    Lab 4 + auth update

---
+++ lab4/files/config-localhost.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wordpress');
+define('DB_PASSWORD', '{{sql_pass}}');
+define('DB_HOST', 'localhost');
+define('WP_CONTENT_DIR', '/usr/share/wordpress/wp-content');
+?>
