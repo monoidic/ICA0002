2bb746213790e5a686a492bf337dd1159b14b16a
Mikk Margus Möll <mimoll@ttu.ee>
2019-12-09 16:49:07 +0200

    Lab 9

---
+++ lab9/roles/wordpress/templates/config-sql.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wordpress');
+define('DB_PASSWORD', '{{sql_pass}}');
+define('DB_HOST', '{{db_host}}');
+define('WP_CONTENT_DIR', '/usr/share/wordpress/wp-content');
+?>
